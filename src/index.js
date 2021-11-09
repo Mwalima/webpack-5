ae72642dd571829752a05bfbfff25e2ab5cc4879
peltemm <mmuc.peltenburg@pzh.nl>
2021-11-09 20:09:37 +0100

    webpack init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const elvenShieldRecipe = {
+    leatherStripes: 10,
+    metalRims: 10,
+    moonStones: 4
+}
+
+console.log(elvenShieldRecipe)
+
+// to check is website is running try
+// node dist/main.js
